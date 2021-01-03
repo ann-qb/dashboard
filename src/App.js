cc04d7806ede4d24106ad7872e3a761b790eb3fd
Ann Susan <anns@qburst.com>
2021-01-03 17:24:47 +0530

    Skeleton implementation of login functionality

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import './App.css';
 
 // import DisplayMessage from './pages/Login/DisplayMessage';
 import GlobalStyle from './GlobalStyleSheet/globalStyleSheet';
-import styled from 'styled-components'
-import LoginPage from './pages/Login'
-import Card from './pages/Login/DisplayMessage'
-
-
+import styled from 'styled-components';
+import LoginPage from './pages/Login';
+import Card from './pages/Login/DisplayMessage';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 function App() {
-
 	return (
 		<div>
-      <GlobalStyle />
-      <LoginPage/>
+			<GlobalStyle />
+			<Router>
+				<Switch>
+					<Route path={'/'} exact>
+						<LoginPage />
+					</Route>
+					<Route path={'/login'}>
+						<LoginPage />
+					</Route>
+					<Route path={'/dashboard'}></Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 }
-
 
 export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,98 @@
 import LoginCard from './LoginCards';
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import DisplayMessage from './DisplayMessage/displayMessage';
+import { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 export default function Login(props) {
-
+	const [userName, setUserName] = useState('');
+	const [password, setPassword] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+	// Get from store
+	const status = 'Active';
+	const validateValue = (cardHeader, fieldValue) => {
+		console.log(cardHeader, fieldValue);
+		if (cardHeader === 'Username') {
+			//regex validation
+			if (fieldValue.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+				//api call
+				const checkUserNameExists = async () => {
+					try {
+						const response = await axios.post(
+							'https://f2066c2c-3359-41dd-a269-a14d820ac25a.mock.pstmn.io/tempmockserver/userexists',
+							{
+								username: fieldValue,
+							}
+						);
+						if (response.status === 200) {
+							console.log(response.data);
+							// dispatch to store
+							setUserName(fieldValue);
+						} else {
+							console.log('Something went wrong username');
+						}
+					} catch (error) {
+						console.log(error);
+					}
+				};
+				checkUserNameExists();
+			} else {
+				setErrorMessage('Please enter a valid email id');
+			}
+		} else if (cardHeader === 'Password') {
+			const loginAPICall = async () => {
+				try {
+					const response = await axios.post(
+						'https://f2066c2c-3359-41dd-a269-a14d820ac25a.mock.pstmn.io/tempmockserver/login',
+						{
+							username: userName,
+							password: fieldValue,
+						}
+					);
+					if (response.status === 200) {
+						console.log(response.data);
+						// dispatch to store
+						setPassword(fieldValue);
+					} else {
+						console.log('Password is incorrect/ Something went wrong pswd');
+					}
+				} catch (error) {
+					console.log(error);
+				}
+			};
+			loginAPICall();
+		}
+	};
 	return (
 		<div>
-			<Header/>
-			<LoginCard cardHeader="Username" cardType="text" buttonText="Next" />
-			<Footer/>
+			<Header />
+
+			{!userName ? (
+				<LoginCard
+					cardHeader="Username"
+					cardType="text"
+					errorMessage={errorMessage}
+					buttonText="Next"
+					onSubmit={validateValue}
+				/>
+			) : status === 'Active' && !password ? (
+				<LoginCard cardHeader="Password" cardType="password" buttonText="Submit" onSubmit={validateValue} />
+			) : status === 'Pending' ? (
+				<DisplayMessage message="Pending" />
+			) : status === 'Inactive' ? (
+				<DisplayMessage message="Inactive" />
+			) : status === 'Active' && password ? (
+				<Redirect to={'/dashboard'} />
+			) : (
+				<DisplayMessage message="User does not exist" />
+			)}
+
+			{/* <LoginCard cardHeader="Password" cardType="new password" buttonText="Save password" /> */}
+			{/* <LoginCard cardHeader="OTP" cardType="text" buttonText="Submit" /> */}
+
+			<Footer />
 		</div>
 	);
 }
