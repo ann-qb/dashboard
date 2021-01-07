a0abebe55ef477377c6451b0fb9ed6a7a8ecfffb
Ann Susan <anns@qburst.com>
2021-01-08 04:53:56 +0530

    Modify loginSlice

--- src/slices/login.slice.js
+++ src/slices/login.slice.js
@@ -1,19 +1,48 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = { loggedIn: false };
+const initialState = { verifiedUser: null, loggedUser: null, role: null, status: 'idle' };
 
 const loginSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        login(state) {
-            state.loggedIn = true;
-        },
-        logout(state) {
-            state.loggedIn = false;
-        },
-    },
+	name: 'user',
+	initialState,
+	reducers: {
+		login(state, action) {
+			state.loggedUser = action.payload.loggedUser;
+			state.role = action.payload.role;
+			state.status = 'success';
+		},
+		logout(state) {
+			state = initialState;
+		},
+		resetStatus(state) {
+			state.status = 'idle';
+		},
+		updateStatus(state, action) {
+			state.status = action.payload.status;
+		},
+	},
 });
 
-export const { login, logout } = loginSlice.actions;
+export const { login, logout, reset, updateStatus } = loginSlice.actions;
 export default loginSlice.reducer;
+
+export const onLogin = (data) => async (dispatch) => {
+	// reset
+	dispatch(reset());
+	// dispatch loading start
+	dispatch(updateStatus('loading'));
+	// try-catch fetch API
+
+	// dispatch loading end
+	// update status
+};
+export const onVerifyUserName = (data) => (dispatch) => {
+	// reset
+	dispatch(reset());
+	// dispatch loading start
+	dispatch(updateStatus('loading'));
+	// try-catch fetch API
+
+	// dispatch loading end
+	// update status
+};
