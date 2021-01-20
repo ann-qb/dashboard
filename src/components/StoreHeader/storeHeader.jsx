df595a586b2f2df3616f38222ff3d9e324aa47eb
Thejus5 <thejuss@qburst.com>
2021-01-20 16:06:03 +0530

    click effect on cart icon

--- src/components/StoreHeader/storeHeader.jsx
+++ src/components/StoreHeader/storeHeader.jsx
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 import Logo from '../../assets/Images/logo_white.png';
 import ProfilePic from '../../assets/Images/profilePic_small.png';
-import DropdownMenu from '../Header/DropdownMenu'
-import SubCategoryDropdown from './SubCategoryDropdown'
-import { useState,useEffect } from 'react';
+import DropdownMenu from '../Header/DropdownMenu';
+import SubCategoryDropdown from './SubCategoryDropdown';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import Badge from '@material-ui/core/Badge';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(() => ({
+	root: {
+		cursor:'pointer'
+	},
+}));
 
 const TopNavigation = styled.div`
 	display: flex;
@@ -20,7 +26,6 @@ const TopNavigation = styled.div`
 	background-color: #5673e8; ;
 `;
 
-
 const LogoWrapper = styled.div`
 	width: 10%;
 	height: fit-content;
@@ -28,7 +33,7 @@ const LogoWrapper = styled.div`
 const LogoImage = styled.img`
 	height: 40px;
 	width: auto;
-	cursor:pointer;
+	cursor: pointer;
 `;
 const SearchWrapper = styled.div`
 	position: relative;
@@ -72,75 +77,74 @@ const CartWrapper = styled.div`
 	padding: 5px;
 	margin-right: 0;
 	color: #fff;
+	cursor:pointer !important;
 `;
 const BottomNavigation = styled.div`
-	position:relative;
+	position: relative;
 	display: flex;
 	align-items: center;
 	height: 40px;
 	width: 100%;
 	padding: 25px;
-	background-color: #fff; 
-	border-bottom:1px solid #eee;
+	background-color: #fff;
+	border-bottom: 1px solid #eee;
 `;
 
 const CategoryLinkWrapper = styled.div`
-	display:flex;
-	align-items:center;
-	width:fit-content;
-	height:100%;
+	display: flex;
+	align-items: center;
+	width: fit-content;
+	height: 100%;
 	margin-right: 30px;
 `;
 
 const CategoryLinks = styled.p`
 	cursor: pointer;
 	color: #000;
-	transition:all 0.2s ease;
+	transition: all 0.2s ease;
 	&:hover {
 		color: #5673e8;
 		border-bottom: 1px solid #5673e8;
 	}
 `;
 
-
-
 export default function StoreHeader(props) {
-	const history = useHistory()
-	const [productsInCart, setProductsInCart] = useState(null)
-	const [subCategoryData, setSubCategoryData] = useState(null)
-	const [subCategoryDropdownOpen, setSubCategoryDropdownOpen] = useState(false)
+	const classes= useStyles()
+	const history = useHistory();
+	const [productsInCart, setProductsInCart] = useState(null);
+	const [subCategoryData, setSubCategoryData] = useState(null);
+	const [subCategoryDropdownOpen, setSubCategoryDropdownOpen] = useState(false);
 
 	useEffect(() => {
-		try{
-			setProductsInCart(localStorage.getItem('productsInCart'))
+		try {
+			setProductsInCart(localStorage.getItem('productsInCart'));
+		} catch {
+			setProductsInCart(null);
 		}
-		catch{
-			setProductsInCart(null)
-		}
-		console.log('...',productsInCart)
-	},[props.itemsInCart])
+		console.log('...', productsInCart);
+	}, [props.itemsInCart]);
 
 	// Master back button (logo button)
-	const backToHome = ()=>{
-		history.push('/store')
-	}
+	const backToHome = () => {
+		history.push('/store');
+	};
 
-	const openSubCategoryDropdown = (e)=>{
-		setSubCategoryData(e.target.innerHTML)
-		setSubCategoryDropdownOpen(true)
-	}
+	const openSubCategoryDropdown = (e) => {
+		setSubCategoryData(e.target.innerHTML);
+		setSubCategoryDropdownOpen(true);
+	};
 	const closeSubCategoryDropdown = () => {
 		setSubCategoryData(null);
 		setSubCategoryDropdownOpen(false);
 	};
 
-		const dropdownActions = (e) => {
-			const clickedDiv = e.target.closest('div');
-			if (clickedDiv.id === 'users') {
-				history.push('/users');
-			} 
-		};
-	
+	const dropdownActions = (e) => {
+		const clickedDiv = e.target.closest('div');
+		if (clickedDiv.id === 'users') {
+			history.push('/users');
+		}
+	};
+
 	return (
 		<>
 			<TopNavigation>
@@ -156,8 +160,8 @@ export default function StoreHeader(props) {
 
 					<DropdownMenu menuHeader="Thejus" role="USER" action={dropdownActions} page="store" />
 				</ProfileWrapper>
-				<CartWrapper>
-					<Badge badgeContent={productsInCart} color="primary">
+				<CartWrapper onClick={()=>alert('Hi')}>
+					<Badge badgeContent={productsInCart} color="primary" className={classes.root}>
 						<ShoppingCartOutlinedIcon />
 					</Badge>
 				</CartWrapper>
