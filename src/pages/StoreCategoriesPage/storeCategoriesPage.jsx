f376d1a90cc3d2f9a5ca834ee349de3decd39c65
Thejus5 <thejuss@qburst.com>
2021-01-28 15:46:26 +0530

    Handle absence of data in store categories page

--- src/pages/StoreCategoriesPage/storeCategoriesPage.jsx
+++ src/pages/StoreCategoriesPage/storeCategoriesPage.jsx
@@ -7,6 +7,22 @@ import Pagination from '@material-ui/lab/Pagination';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { onGetStoreProductListing } from '../../slices/storeproductlisting.slice';
+import ShowIfAuth from '../../components/ShowIfAuth'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+	button: {
+		backgroundColor: '#5673E8',
+		marginTop:'20px',
+		color: '#fff',
+		'&:hover': {
+			transition: '0.2s ease',
+			color: '#000',
+			backgroundColor: '#d6dcf9',
+		},
+	},
+}));
 
 const ContentWrapper = styled.div`
 	display: flex;
@@ -35,12 +51,23 @@ const ProductCardWrapper = styled.div`
 	display: flex;
 	flex-wrap: wrap;
 	width: 100%;
+	min-height: 80vh;
 	padding: 10px 0;
+`;
+
+const NoProductMessageWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	height: fit-content;
+	margin: 20px 0;
 `;
 
 export default function StoreCategoriesPage(props) {
 	const history = useHistory();
 	const location = useLocation();
+	const classes = useStyles();
 
 	const useQuery = () => {
 		return new URLSearchParams(location.search);
@@ -61,6 +88,9 @@ export default function StoreCategoriesPage(props) {
 		}
 	}, [category, subCategory]);
 
+	const redirectToAddProductsPage = ()=>{
+		history.push('/addProducts')
+	}
 	return (
 		<>
 			<StoreHeader />
@@ -83,10 +113,24 @@ export default function StoreCategoriesPage(props) {
 					</p>
 					<SectionHeading>{subCategory}</SectionHeading>
 					<ProductCardWrapper>
+						{productListing.length === 0 ? (
+							<NoProductMessageWrapper>
+								<p>Sorry, No Products Available</p>
+								<ShowIfAuth allowedRoles={['admin']}>
+									<Button
+										variant="contained"
+										className={classes.button}
+										disableElevation
+										onClick={redirectToAddProductsPage}
+									>
+										Add Products
+									</Button>
+								</ShowIfAuth>
+							</NoProductMessageWrapper>
+						) : null}
 						{productListing.map((each) => (
 							<ProductCards key={each.id + each.name} data={each} margin="5px 5px" />
 						))}
-						
 					</ProductCardWrapper>
 					<Pagination count={10} shape="rounded" />
 				</ProductsWrapper>
