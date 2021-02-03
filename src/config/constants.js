8ed941f3bb10699282ec676f77463b329c4dd590
Thejus5 <thejuss@qburst.com>
2021-02-03 14:26:37 +0530

    convert to ssl

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const baseURL = 'http://user-dashboard.qburst.build:3002';
+export const baseURL = 'https://user-dashboard.qburst.build:3002';
 export const baseRedirectURL = 'http://localhost:3001';
-export const baseImageURL = 'http://user-dashboard.qburst.build/user_dashboard';
+export const baseImageURL = 'https://user-dashboard.qburst.build/user_dashboard';
